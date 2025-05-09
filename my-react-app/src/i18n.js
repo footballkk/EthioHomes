ff84be8ff8f44b1ba83c3bbe14c845ead08877ea
Redwan Seid <159645769+footballkk@users.noreply.github.com>
2025-05-08 18:03:52 -0700

    yes some some change

--- my-react-app/src/i18n.js
+++ my-react-app/src/i18n.js
@@ -1,9 +1,9 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import en from './locales/en/translation.json';
-import am from './locales/am/translation.json';
-import om from './locales/om/translation.json';
+import en from '../locales/en/translation.json';
+import am from '../locales/am/translation.json';
+import om from '../locales/om/translation.json';
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
