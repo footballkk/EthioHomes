55cbb8389505ce0e26ba18dd45885024e25cce78
Redwan Seid <159645769+footballkk@users.noreply.github.com>
2025-05-08 18:01:31 -0700

    some bold updates

--- my-react-app/src/i18n.js
+++ my-react-app/src/i18n.js
@@ -1,20 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-
+import LanguageDetector from 'i18next-browser-languagedetector';
+import en from './locales/en/translation.json';
+import am from './locales/am/translation.json';
+import om from './locales/om/translation.json';
 i18n
-  .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: 'en',
-    fallbackLng: 'en',
-    debug: false,
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json'  // This path is served from /public
+    resources: {
+      en: { translation: en },
+      am: { translation: am },
+      om: { translation: om }
     },
+    fallbackLng: 'en',
     interpolation: {
       escapeValue: false
     }
   });
-
 export default i18n;
