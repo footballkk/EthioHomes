245e2624116abad2ea65ab3bd14d4c60144c5a2a
Redwan Seid <159645769+footballkk@users.noreply.github.com>
2025-05-15 15:27:53 -0700

    un done the change

--- my-react-app/src/components/MessageForm.jsx
+++ my-react-app/src/components/MessageForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-console.log("Props received in MessageForm:", { senderId, receiverId, propertyId });
+// console.log("Props received in MessageForm:", { senderId, receiverId, propertyId });
 const MessageForm = ({ senderId, receiverId, propertyId }) => {
   const [text, setText] = useState('');
 
